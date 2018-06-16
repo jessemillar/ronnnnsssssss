c9d1953dde9b52d9d66c8d83afbb0788fa8de741
Jesse Millar <hellojessemillar@gmail.com>
2018-06-16 13:22:21 -0500

    Getting close to fully functional

--- app/index.js
+++ app/index.js
@@ -1,20 +1,39 @@
 import clock from "clock";
+import { today } from "user-activity";
 import document from "document";
-import { preferences } from "user-settings";
 import * as util from "../common/utils";
 
 clock.granularity = "minutes";
 
+// Time digit positions
 let pos1 = {x: -8, y: 66};
 let pos2 = {x: 54, y: 60};
 let pos3 = {x: 102, y: 60};
 let pos4 = {x: 152, y: 58};
 let pos5 = {x: 218, y: 56};
-
 let pos1b = {x: 24, y: 60};
 let pos2b = {x: 72, y: 60};
 let pos3b = {x: 122, y: 58};
 let pos4b = {x: 188, y: 56};
+
+let day = document.getElementById("day");
+
+let dateDigits = [
+  document.getElementById("date-1"),
+  document.getElementById("date-2"),
+  document.getElementById("date-3"),
+  document.getElementById("date-4"),
+  document.getElementById("date-5")
+];
+
+let stepDigits = [
+  document.getElementById("steps-1"),
+  document.getElementById("steps-2"),
+  document.getElementById("steps-3"),
+  document.getElementById("steps-4"),
+  document.getElementById("steps-5"),
+  document.getElementById("steps-6")
+];
 
 let colon = document.getElementById("colon");
 let hourLeft = document.getElementById("hour-left");
@@ -22,14 +41,18 @@ let hourRight = document.getElementById("hour-right");
 let minuteLeft = document.getElementById("minute-left");
 let minuteRight = document.getElementById("minute-right");
 
-// Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
-  let today = evt.date;
-  let hour = util.zeroPad(today.getHours() - 3 % 12 || 12);
-  let minute = util.zeroPad(today.getMinutes());
-    
-  if (hour > 9) {
-    util.setLargeDigitValue(hourLeft, ("" + hour)[0]);
+clock.ontick = (evt) => {  
+  let date = evt.date;
+  let curDay = date.getDay();
+  let curHour = util.zeroPad(date.getHours() % 12 || 12);
+  let curMinute = util.zeroPad(date.getMinutes());
+
+  util.setDayValue(day, curDay);
+  util.drawDate(dateDigits, date.getMonth(), date.getDate());
+  util.drawSteps(stepDigits, today.local.steps || 0);
+  
+  if (curHour > 9) {
+    util.setLargeDigitValue(hourLeft, ("" + curHour)[0]);
     
     util.setDigitPosition(hourLeft, pos1);
     util.setDigitPosition(hourRight, pos2);
@@ -45,8 +68,8 @@ clock.ontick = (evt) => {
     util.setDigitPosition(minuteRight, pos4b);
   }
   
-  util.setLargeDigitValue(hourRight, ("" + hour)[1]);
+  util.setLargeDigitValue(hourRight, ("" + curHour)[1]);
   
-  util.setLargeDigitValue(minuteLeft, ("" + minute)[0]);
-  util.setLargeDigitValue(minuteRight, ("" + minute)[1]);
+  util.setLargeDigitValue(minuteLeft, ("" + curMinute)[0]);
+  util.setLargeDigitValue(minuteRight, ("" + curMinute)[1]);
 }
--- common/utils.js
+++ common/utils.js
@@ -6,11 +6,69 @@ export function zeroPad(i) {
   return i;
 }
 
+export function drawDate(dateDigits, month, day) {
+  let month = ("" + month++);
+  let day = ("" + day);
+  
+  if (month.length > 1) {
+    setSmallDigitValue(dateDigits[0], month[0]);
+    setSmallDigitValue(dateDigits[1], month[1]);
+    setSmallDigitValue(dateDigits[2], "Dash");
+  } else {
+    setSmallDigitValue(dateDigits[0], month[0]);
+    setSmallDigitValue(dateDigits[1], "Dash");
+  }
+}
+
+export function drawSteps(stepDigits, steps) {
+  let steps = ("" + steps);
+      
+  for (var i = steps.length; i > 0; i--) {
+    setSmallDigitValue(stepDigits[i], steps[i]);
+  }
+  
+  setSmallDigitValue(stepDigits[stepDigits.length - 1 - steps.length], "Dollar");
+  
+  for (var i = 0; i < stepDigits.length - 1 - steps.length; i++) {
+    stepDigits[i].style.display = "none";
+  }
+}
+
 export function setDigitPosition(digit, pos) {
   digit.x = pos.x;
   digit.y = pos.y;
 }
 
+export function setSmallDigitValue(digit, val) {
+  digit.image = `small-${val}.png`;
+}
+
 export function setLargeDigitValue(digit, val) {
   digit.image = `large-${val}.png`;
+}
+
+export function setDayValue(day, val) {
+  switch(val) {
+    case 0:
+      day.image = `small-Sun.png`;
+      break;
+    case 1:
+      day.image = `small-Mon.png`;
+      break;
+    case 2:
+      day.image = `small-Tue.png`;
+      break;
+    case 3:
+      day.image = `small-Wed.png`;
+      break;
+    case 4:
+      day.image = `small-Thu.png`;
+      break;
+    case 5:
+      day.image = `small-Fri.png`;
+      break;
+    case 6:
+      day.image = `small-Sat.png`;
+      break;
+  }
 }
